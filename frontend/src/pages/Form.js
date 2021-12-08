da1e5be6e1437218c33036a442c44e2eed4410f1
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-12-08 19:50:58 +0100

    modif

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Form = () => {
-  const [name, setName] = useState("")
-
+    const [name, setName] = useState("")
+    const [iSValid, setIsValid] = useState(false)
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -23,15 +23,26 @@ const Form = () => {
     })
       .then(response => response.json())
       .then(data => console.log(data))
+      setIsValid(true)
   }
 
   return (
+      <>
+       {iSValid ?
+       (
+        <>
+        <h1>Yeah added student</h1>
+        <Link to="/"><p>retour au menu</p></Link>
+        </>
+      ):
     <form onSubmit={handleSubmit}>
       <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
      
       <button type="submit">Creer student</button>
     </form>
   
+    }
+  </>
   );
 };
 
